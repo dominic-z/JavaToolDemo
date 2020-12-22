63614fc2099c8c75b9ba0763e4502bc240e2c8cd
dominiczhu <dominiczhu@tencent.com>
2020-12-22 09:20:43 +0800

    update Lombok

---
+++ src/test/java/Demo.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @title Demo
+ * @Author dominiczhu
+ * @Date 2020/12/22 上午9:18
+ * @Version 1.0
+ */
+public class Demo {
+    @Test
+    public void testMin(){
+        System.out.println(0-Integer.MIN_VALUE);
+    }
+}
