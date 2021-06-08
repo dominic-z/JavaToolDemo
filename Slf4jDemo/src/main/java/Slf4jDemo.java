37fd6380cee9d250c595691e6a1e1adc50f4c48c
dominiczhu <dominiczhu@tencent.com>
2021-06-08 10:50:52 +0800

    log error

--- Slf4jDemo/src/main/java/Slf4jDemo.java
+++ Slf4jDemo/src/main/java/Slf4jDemo.java
@@ -8,9 +8,10 @@ import org.slf4j.LoggerFactory;
 
 public class Slf4jDemo {
 
-    private static Logger logger= LoggerFactory.getLogger("mylog");
+    private static Logger logger = LoggerFactory.getLogger("mylog");
 
-    private static Logger classLogger=LoggerFactory.getLogger(Slf4jDemo.class);
+    private static Logger classLogger = LoggerFactory.getLogger(Slf4jDemo.class);
+
     public static void main(String[] args) {
         System.out.println("==============服从自定义命名的logger的配置");
         // 记录trace级别的信息
@@ -21,6 +22,7 @@ public class Slf4jDemo {
         logger.info("log4j2日志输出：This is info message.");
         // 记录error级别的信息
         logger.error("log4j2日志输出：This is error message.");
+        logger.error("{} log4j2日志输出：This is error message.", "[fuck]", throwRuntimeExp());
 
         System.out.println("==============类logger服从root的配置");
         // 记录trace级别的信息
@@ -31,5 +33,9 @@ public class Slf4jDemo {
         classLogger.info("log4j2日志输出：This is info message.");
         // 记录error级别的信息
         classLogger.error("log4j2日志输出：This is error message.");
+    }
+
+    private static Exception throwRuntimeExp() {
+        return new RuntimeException("run time");
     }
 }
