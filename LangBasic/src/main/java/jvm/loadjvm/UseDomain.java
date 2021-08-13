5ef73a381d02addd554b5ea528bf7a6724b9c84b
dominiczhu <dominiczhu@tencent.com>
2021-08-13 11:17:19 +0800

    挪动类路径

---
+++ LangBasic/src/main/java/jvm/loadjvm/UseDomain.java
@@ -0,0 +1,17 @@
+package jvm.loadjvm;
+
+import jvm.loadjvm.domain.Banana;
+
+/**
+ * @author dominiczhu
+ * @version 1.0
+ * @title UseDomain
+ * @date 2021/8/13 上午10:26
+ */
+public class UseDomain {
+
+    public static void main(String[] args) {
+        Banana banana = new Banana();
+        System.out.println(banana);
+    }
+}
