aa266eef5d96649b92ac057b52c48e17db3266de
dominiczhu <dominiczhu@tencent.com>
2021-09-02 20:12:30 +0800

    继承

--- LangBasic/src/main/java/polymorphic/domain/Apple.java
+++ LangBasic/src/main/java/polymorphic/domain/Apple.java
@@ -8,22 +8,23 @@ package polymorphic.domain;
  */
 public class Apple extends Fruit {
 
-    public String name = "apple";
+    public int name = 1;
 
     // 重写方法的时候，子类方法的返回类型可以是父类方法返回类型的子类
     @Override
     public String get() {
-        return this.name;
+        return String.valueOf(this.name);
     }
 
-    private void showSupper(){
+    private void showSupper() {
 
-        // 下面这俩name根本就是俩东西，变量这东西不存在覆盖什么的
+        // 如果访问同名字段，那么子类字段会将父类字段隐藏掉
         System.out.println(super.name);
         System.out.println(this.name);
     }
 
     public static void main(String[] args) {
         System.out.println(new Apple());
+        new Apple().showSupper();
     }
 }
